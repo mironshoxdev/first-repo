a7652c20af3cfec6ff7938f51e783e59dc8ec354
mironshoxdev <mironshoxoxunjonov@gmail.com>
2024-05-29 15:56:16 +1300

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import LoginAuth from './components/crud'
+
+
+const App = () => {
+  return (
+    <div>
+      <LoginAuth/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+
+    <ToastContainer
+      position="top-center"
+      autoClose={1999}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
+  </React.StrictMode>
+);
